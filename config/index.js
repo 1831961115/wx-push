8af33e6137cbff2659f1ccbb35343f585a2ac91e
1831961115 <112805932+1831961115@users.noreply.github.com>
2025-05-11 12:22:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx534b2639aad405e3",
+  appsecret: "4d6965f0ae4a236c4d3fdc5449c4ad3a",
+  template_id: "YVxMTlHTpnJLJR8wlhgsZy8alQrtRxH_O1dSzIdhyJs",
+  touser: "oxue67aQqi_gNOrLK4vbu1jRvYQ0",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "洛阳",
+  first_date: "2025-04-24",
 };
